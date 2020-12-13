78e009f8ee5573c46dfe238d29485be688e50f28
guohaiqiang <837557495@qq.com>
2020-12-13 21:25:08 +0800

    配置处理

--- App.vue
+++ App.vue
@@ -6,17 +6,17 @@ export default {
 	onShow: function() {
 		console.log('App Show');
 
-		var ua = navigator.userAgent.toLowerCase();
-		var isWeixin = ua.indexOf('micromessenger') != -1;
-		if (!isWeixin) {
-			console.log('我是谁')
-		 	// window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' + this.$appid; // 自己的appid
+		// var ua = navigator.userAgent.toLowerCase();
+		// var isWeixin = ua.indexOf('micromessenger') != -1;
+		// if (!isWeixin) {
+		// 	console.log('我是谁')
+		//  	window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' + this.$appid; // 自己的appid
 
-			// 也可以跳转到自己写的错误页面
-			// uni.reLaunch({
-			// 	url:'/pages/address/address'
-			// })
-		}
+		// 	也可以跳转到自己写的错误页面
+		// 	uni.reLaunch({
+		// 		url:'/pages/address/address'
+		// 	})
+		// }
 	},
 	onHide: function() {
 		console.log('App Hide');
